78b4e072857e20ae6f527e4fb4861ff95a7ddbb5
Usi Diamond <UsiDiamond@proton.me>
2023-06-16 06:09:57 -0400

    exit server and karma config

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // Create link to Angular build directory
 // The `ng build` command will save the result
 // under the `dist` folder.
-var distDir = __dirname + "dist/neurodiversity-main/";
+var distDir = __dirname + "/public/neurodiversity-main/";
 app.use(express.static(distDir));
 
 function getRoot(request, response) {
