4fd6f7d051eaf2bd5697050cc5d4433c80338dd5
Usi Diamond <UsiDiamond@proton.me>
2023-07-07 13:49:35 -0400

    use ngoninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'neurodiversity-main';
+
+  ngOnInit(){
+    window.location.href = "https://neurodiversityinbusiness.org/";
+
+  }
 }
