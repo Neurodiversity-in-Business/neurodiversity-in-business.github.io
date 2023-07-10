afa8c49f1bd91d6d1eac5e3a4082a32e8f97c37e
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-07-10 03:56:20 -0400

    Development (#118)
    * fix scss
    
    * fix mix of css and scss
    
    * remove unused font awesome imports
    
    * remove unused style
    
    * fix scss build
    
    * try reverting to css
    
    * remove add scss files
    
    * fix css file
    
    * Switch to release branches (#117) (#119)
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    Co-authored-by: Usi Diamond <UsiDiamond@proton.me>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { startWith } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'Neurodiversity in Business';
