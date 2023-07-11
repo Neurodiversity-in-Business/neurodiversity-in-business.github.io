afffbca9afc3426fe479ad1398fc9d8bf0eb8119
Usi Diamond <UsiDiamond@proton.me>
2023-07-11 14:31:41 -0400

    fix formatting issuetructure project for future development

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 import {
   NgbCarousel,
   NgbSlideEvent,
   NgbSlideEventSource,
 } from '@ng-bootstrap/ng-bootstrap';
-import { CompanyLogoImage } from './carosels/company-logo-image';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
