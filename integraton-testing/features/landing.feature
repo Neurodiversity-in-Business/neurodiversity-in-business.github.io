c56857e6e0980c3f87c4063f9d30407f2f80edc0
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-07-12 11:06:08 -0400

    Staging (#210)
    * formatting
    
    * make first end to end test
    
    ---------
    
    Co-authored-by: Usi Diamond <UsiDiamond@proton.me>

--- integraton-testing/features/landing.feature
+++ integraton-testing/features/landing.feature
@@ -1,5 +1,6 @@
 Feature: Landing Page
     This feature lets a user see the landing page.
-Scenario: Basic increment scenario
-     Given I go to "http://localhost:8080/"
-     Then click on the Home Button
+    Scenario: Basic increment scenario
+        Given I go to "http://localhost:8080/"
+        Then I click on the Skip to Content link
+        Then the first header has focus
--- integraton-testing/step_definitions/basic.steps.js
+++ integraton-testing/step_definitions/basic.steps.js
@@ -1,15 +1,14 @@
 // Import the cucumber operators we need
 const { Before, Given, When, Then, After } = require("@cucumber/cucumber");
 const { HomePage } = require("../pages/home.po");
-
 var chai = require("chai");
 var expect = chai.expect;
 
 var chaiAsPromised = require("chai-as-promised");
+const { assert } = require("console");
 chai.use(chaiAsPromised);
 
 var expect = chai.expect;
-
 Before(function (scenario, callback) {
   this.homepage = new HomePage();
 
@@ -18,15 +17,14 @@ Before(function (scenario, callback) {
 
 Given("I go to {string}", function (string) {
   this.homepage.navigateToHome();
-  //return ;
-
-  //expect(browser).to.eventually.equal("http://localhost:8080");
 });
-Then("click on the Home Button", function () {
-  browser
-    .waitForElementVisible("#homeContent")
-    .click("#homeContent")
-    .screenshot()
-    .end();
-  //expect(" ").to.be.equal("");
+Then("I click on the Skip to Content link", function () {
+  browser.perform(function () {
+    const actions = this.actions();
+    actions.click("#skipLink").pause(1000);
+    browser.element("#pageHeader").expect.isSelected();
+  });
+});
+Then("the first header has focus", async function () {
+  return browser.pause(1000).end();
 });
