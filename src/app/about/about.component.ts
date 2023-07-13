d10bc8f63d536403381acefc1d7454adcc55de10
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-07-12 22:37:30 -0400

    Staging (#228)
    * automated testing
    
    * chrome headless
    
    * remove test output
    
    * fix navigation on refresh
    
    * finish about us content
    
    ---------
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    Co-authored-by: Usi Diamond <UsiDiamond@proton.me>

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  panels = [
+    'Buisness Drivers',
+    'Societal Impact Drivers',
+    'The Strengths',
+    'Other Diversity & Inclusion',
+  ];
+}
