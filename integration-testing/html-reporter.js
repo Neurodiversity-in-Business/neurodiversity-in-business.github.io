8db44899e0cb070f80162dcd744b131e8159f2f2
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-07-13 18:10:16 -0400

    Staging (#257)
    * automated testing
    
    * chrome headless
    
    * remove test output
    
    * fix navigation on refresh
    
    * finish about us content
    
    * Delete tests_output/nightwatch-html-report directory
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * Update docker-image.yml
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * use reporter
    
    * coveratge report
    
    * Update docker-image.yml
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * fix screen display issue
    
    * Update and rename docker-image.yml to integration-testing.yml
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * fix builds
    
    * Update README.md
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * Update README.md
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * add developer tools
    
    * prune
    
    * Create CODE_OF_CONDUCT.md
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * Update docker-build.yml
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
    
    * cleanup
    
    ---------
    
    Signed-off-by: usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>

--- integration-testing/html-reporter.js
+++ integration-testing/html-reporter.js
@@ -1,15 +1,19 @@
-var fs = require('fs');
-var path = require('path');
-var handlebars = require('handlebars');
+var fs = require("fs");
+var path = require("path");
+var handlebars = require("handlebars");
 
 module.exports = {
   write: function (results, options, done) {
-
-    var reportFilename = options.filename_prefix + (Math.floor(Date.now() / 1000)) + '.html';
-    var reportFilePath = path.join(__dirname, options.output_folder, reportFilename);
+    var reportFilename =
+      options.filename_prefix + Math.floor(Date.now() / 1000) + ".html";
+    var reportFilePath = path.join(
+      __dirname,
+      options.output_folder,
+      reportFilename
+    );
 
     // read the html template
-    fs.readFile('integration-testing/html-reporter.hbs', function (err, data) {
+    fs.readFile("integration-testing/html-reporter.hbs", function (err, data) {
       if (err) throw err;
 
       var template = data.toString();
@@ -19,16 +23,16 @@ module.exports = {
         results: results,
         options: options,
         timestamp: new Date().toString(),
-        browser: options.filename_prefix.split('_').join(' '),
-        totalTests: results.passed + results.failed + results.errors
+        browser: options.filename_prefix.split("_").join(" "),
+        totalTests: results.passed + results.failed + results.errors,
       });
 
       // write the html to a file
       fs.writeFile(reportFilePath, html, function (err) {
         if (err) throw err;
-        console.log('Report generated: ' + reportFilePath);
+        console.log("Report generated: " + reportFilePath);
         done();
       });
     });
-  }
+  },
 };
