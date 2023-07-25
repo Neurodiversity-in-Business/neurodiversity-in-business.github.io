e082997b583642e1c4b0792758083032336e7f86
Usi Diamond <UsiDiamond@proton.me>
2023-07-25 18:16:38 -0400

    hard config push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   msalConfig: {
     auth: {
-      clientId: '6f3bb1f9-e622-4235-a5f7-5a33286d9a4a',
+      clientId: '91cf3203-497a-4d2f-9c21-9f2d37262a4a',
       authority:
         'https://login.microsoftonline.com/b03441e2-ab97-45c2-a6dd-c52e3f5934af',
     },
   },
   apiConfig: {
     scopes: ['user.read'],
-    uri: 'https://neurodiversity-in-buisness.github.io',
+    uri: 'https://neurodiversity-in-business.github.io',
   },
 };
