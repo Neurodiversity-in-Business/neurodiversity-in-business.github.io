223c00b7a038dc6d923c07b0eff8f50a56e2d7f3
Usi Diamond <UsiDiamond@proton.me>
2023-07-25 14:06:12 -0400

    WIP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
+  msalConfig: {
+      auth: {
+          clientId: 'b5c2e510-4a17-4feb-b219-e55aa5b74144',
+          authority: 'https://login.microsoftonline.com/common'
+      }
+  },
+  apiConfig: {
+      scopes: ['user.read'],
+      uri: 'https://graph.microsoft.com/v1.0/me'
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
