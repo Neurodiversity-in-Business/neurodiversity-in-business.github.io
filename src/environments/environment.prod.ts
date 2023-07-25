6ef5039a2ba357ce9cb091c08a7ac2fbcc504fd7
Usi Diamond <UsiDiamond@proton.me>
2023-07-25 18:15:13 -0400

    correct clientid

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   msalConfig: {
     auth: {
-      clientId: '6f3bb1f9-e622-4235-a5f7-5a33286d9a4a',
+      clientId: '91cf3203-497a-4d2f-9c21-9f2d37262a4a',
       authority:
         'https://login.microsoftonline.com/b03441e2-ab97-45c2-a6dd-c52e3f5934af',
     },
