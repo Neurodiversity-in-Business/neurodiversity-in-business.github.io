816a801f1b861a4ca13fee5a971e61d46fb052b9
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-07-25 17:44:39 -0400

    Feature/azure sso (#332)
    * WIP
    
    * working login/logout with issues
    
    * fixed formatting
    
    * Too many imports for reasonable testing
    
    * remove google verification file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,28 +2,52 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  Inject,
+  OnDestroy,
   OnInit,
   ViewChild,
 } from '@angular/core';
 import { Router } from '@angular/router';
 import { DisplayContentService } from './display-content.service';
-
+import {
+  MsalService,
+  MsalBroadcastService,
+  MSAL_GUARD_CONFIG,
+  MsalGuardConfiguration,
+} from '@azure/msal-angular';
+import {
+  AuthenticationResult,
+  InteractionStatus,
+  PopupRequest,
+  RedirectRequest,
+  EventMessage,
+  EventType,
+} from '@azure/msal-browser';
+import { Subject } from 'rxjs/internal/Subject';
+import { filter, takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('nav') nav: ElementRef;
   title = 'Neurodiversity in Business';
   navActiveId = 1;
   isContentShown = true;
   isMenuCollapsed = window.innerWidth <= 991;
+  loginDisplay = false;
+  username = "";
+  private readonly _destroying$ = new Subject<void>();
+
   public isMobileLayout = false;
 
   constructor(
     private displayContentService: DisplayContentService,
-    private router: Router
+    private router: Router,
+    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService
   ) {}
 
   ngOnInit() {
@@ -34,6 +58,41 @@ export class AppComponent implements OnInit, AfterViewInit {
       this.isMobileLayout = window.innerWidth <= 991;
       this.isMenuCollapsed = this.isMobileLayout;
     };
+    this.setLoginDisplay();
+    this.authService.handleRedirectObservable().subscribe((res) => {
+      this.isContentShown = true;
+      this.username  =  this.authService.instance.getActiveAccount()?.name!;
+      console.log(res);
+    });
+
+    this.authService.instance.enableAccountStorageEvents(); // Optional - This will enable ACCOUNT_ADDED and ACCOUNT_REMOVED events emitted when a user logs in or out of another tab or window
+    this.msalBroadcastService.msalSubject$
+      .pipe(
+        filter(
+          (msg: EventMessage) =>
+            msg.eventType === EventType.ACCOUNT_ADDED ||
+            msg.eventType === EventType.ACCOUNT_REMOVED
+        )
+      )
+      .subscribe((result: EventMessage) => {
+        if (this.authService.instance.getAllAccounts().length === 0) {
+          window.location.pathname = '/';
+        } else {
+          this.setLoginDisplay();
+        }
+      });
+
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter(
+          (status: InteractionStatus) => status === InteractionStatus.None
+        ),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay();
+        this.checkAndSetActiveAccount();
+      });
   }
 
   ngAfterViewInit(): void {
@@ -58,7 +117,49 @@ export class AppComponent implements OnInit, AfterViewInit {
       },
     });
   }
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+  }
+  checkAndSetActiveAccount() {
+    /**
+     * If no active account set but there are accounts signed in, sets first account to active account
+     * To use active account set here, subscribe to inProgress$ first in your component
+     * Note: Basic usage demonstrated. Your app may require more complicated account selection logic
+     */
+    let activeAccount = this.authService.instance.getActiveAccount();
 
+    if (
+      !activeAccount &&
+      this.authService.instance.getAllAccounts().length > 0
+    ) {
+      let accounts = this.authService.instance.getAllAccounts();
+      this.authService.instance.setActiveAccount(accounts[0]);
+    }
+  }
+  loginRedirect() {
+    if (this.msalGuardConfig.authRequest) {
+      this.authService.loginRedirect({
+        ...this.msalGuardConfig.authRequest,
+      } as RedirectRequest);
+    } else {
+      this.authService.loginRedirect();
+    }
+  }
+
+  logout(popup?: boolean) {
+    if (popup) {
+      this.authService.logoutPopup({
+        mainWindowRedirectUri: '/',
+      });
+    } else {
+      this.authService.logoutRedirect();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
   toggleContent(flag: string) {
     if (flag == 'hide') {
       this.isContentShown = false;
