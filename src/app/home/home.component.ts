111de1e1c1113a27ecbaa6675b367cbdb7b1f9d2
Usi Diamond <UsiDiamond@proton.me>
2023-07-25 23:57:55 -0400

    working demo of carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import {} from '@ng-bootstrap/ng-bootstrap';
+import { CarouselData } from './carousel-data';
+import { CarouselItem } from './carousel-item';
+import { CarouselService } from '../carousel.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  companyCarouselDataObservable: Observable<CarouselData>;
+  partnerCarouselDataObservable: Observable<CarouselData>;
+  carouselIsReady:boolean = true;
+  constructor(private _carouselService: CarouselService) {}
+  ngOnInit(): void {
+    this.companyCarouselDataObservable =
+      this._carouselService.corporateCarouselObservable$;
+    this.partnerCarouselDataObservable =
+      this._carouselService.partnercorpCarouselObservable$;
+
+    const corporateData = new CarouselData();
+    corporateData.directory= "corporation";
+    corporateData.items = [];
+    corporateData.items.push(new CarouselItem('Oracle', 'oracle.svg'));
+
+    this._carouselService.corpSubject.next(corporateData);
+    this.carouselIsReady = true;
+  }
+}
---
+++ src/app/home/organization-carousel/organization-carousel.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CarouselData } from '../carousel-data';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-organization-carousel',
+  templateUrl: './organization-carousel.component.html',
+  styleUrls: ['./organization-carousel.component.css'],
+})
+export class OrganizationCarouselComponent implements OnInit {
+  @Input('carouselDataObservable') carouselDataObervable: Observable<CarouselData>;
+  carouselData: CarouselData;
+  ngOnInit(): void {
+    this.carouselDataObervable.subscribe((data) => {
+      console.log(data);
+      this.carouselData = data;
+    });
+  }
+}
