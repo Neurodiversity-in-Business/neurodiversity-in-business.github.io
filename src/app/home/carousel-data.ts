d6f37a96e90a3989c63a7ab85a3d23be756588b9
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-07-26 01:02:52 -0400

    Feature/carousels (#340)
    * working demo of carousel
    
    * presentable example
    
    * remove unneeded tests
    
    * remove scss file
    
    * presentable with examples
    
    * get rid of 508 bad thingies

---
+++ src/app/home/carousel-data.ts
@@ -0,0 +1,7 @@
+import { CarouselItem } from "./carousel-item";
+
+export class CarouselData {
+    title:string;
+    directory:string;
+    items:Array<CarouselItem>;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import {} from '@ng-bootstrap/ng-bootstrap';
+import { CarouselData } from './carousel-data';
+import { CarouselItem } from './carousel-item';
+import { CarouselService } from '../carousel.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  companyCarouselDataObservable: Observable<CarouselData>;
+  partnerCarouselDataObservable: Observable<CarouselData>;
+  carouselIsReady: boolean = false;
+  constructor(private _carouselService: CarouselService) {}
+  ngOnInit(): void {
+    this.companyCarouselDataObservable =
+      this._carouselService.corporateCarouselObservable$;
+    this.partnerCarouselDataObservable =
+      this._carouselService.partnercorpCarouselObservable$;
+
+    const corporateData = new CarouselData();
+    corporateData.directory = 'corporation';
+    corporateData.items = [];
+    corporateData.items.push(new CarouselItem('Oracle', 'oracle.svg'));
+    corporateData.items.push(new CarouselItem('Capita', 'capita.svg'));
+    corporateData.items.push(new CarouselItem('Heathrow', 'heathrow.svg'));
+
+    this._carouselService.corpSubject.next(corporateData);
+
+
+    const partnerData = new CarouselData();
+    partnerData.directory = 'community';
+    partnerData.items = [];
+    partnerData.items.push(new CarouselItem('British Dyslexia Association', 'bda.png'));
+    partnerData.items.push(new CarouselItem('Ambitious About Autism', 'aba.png'));
+    partnerData.items.push(new CarouselItem('Diversity and Ability', 'danda.jpeg'));
+
+    this._carouselService.partSubject.next(partnerData);
+    this.carouselIsReady = true;
+
+
+  }
+}
