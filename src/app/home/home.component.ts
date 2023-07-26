4f7585054e48715b04d5b89ffacfd3c691c2854e
Usi Diamond <UsiDiamond@proton.me>
2023-07-26 00:44:49 -0400

    presentable with examples

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,7 @@ import { CarouselService } from '../carousel.service';
 export class HomeComponent implements OnInit {
   companyCarouselDataObservable: Observable<CarouselData>;
   partnerCarouselDataObservable: Observable<CarouselData>;
-  carouselIsReady:boolean = true;
+  carouselIsReady: boolean = false;
   constructor(private _carouselService: CarouselService) {}
   ngOnInit(): void {
     this.companyCarouselDataObservable =
@@ -22,13 +22,25 @@ export class HomeComponent implements OnInit {
       this._carouselService.partnercorpCarouselObservable$;
 
     const corporateData = new CarouselData();
-    corporateData.directory= "corporation";
+    corporateData.directory = 'corporation';
     corporateData.items = [];
     corporateData.items.push(new CarouselItem('Oracle', 'oracle.svg'));
     corporateData.items.push(new CarouselItem('Capita', 'capita.svg'));
     corporateData.items.push(new CarouselItem('Heathrow', 'heathrow.svg'));
 
     this._carouselService.corpSubject.next(corporateData);
+
+
+    const partnerData = new CarouselData();
+    partnerData.directory = 'community';
+    partnerData.items = [];
+    partnerData.items.push(new CarouselItem('British Dyslexia Association', 'bda.png'));
+    partnerData.items.push(new CarouselItem('Ambitious About Autism', 'aba.png'));
+    partnerData.items.push(new CarouselItem('Diversity and Ability', 'danda.jpeg'));
+
+    this._carouselService.partSubject.next(partnerData);
     this.carouselIsReady = true;
+
+
   }
 }
