c9bf23821ced47d333aefcc4a8e8e6baf4d40f15
Usi Diamond <UsiDiamond@proton.me>
2023-08-07 23:52:19 -0400

    Scaffolding For Resouce Hub

--- src/app/hub/hub.component.ts
+++ src/app/hub/hub.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../firebase.service';
+import { Resource } from '../resource';
 
 @Component({
   selector: 'app-hub',
   templateUrl: './hub.component.html',
   styleUrls: ['./hub.component.css'],
 })
-export class HubComponent {}
+export class HubComponent implements OnInit {
+  resourcesList:Array<Resource> = [];
+
+  constructor(private _firebaseService: FirebaseService) {}
+
+  ngOnInit(): void {
+    const $this= this;
+    this._firebaseService.getStaticData().filter((item)=>$this.testFilters(item)).forEach((data:Resource) => {
+      this.resourcesList.push(data);
+    });
+
+  }
+
+  private testFilters(item: Resource): boolean {
+
+    return true;
+  }
+}
---
+++ src/app/resource.ts
@@ -0,0 +1,17 @@
+export class Resource {
+    source: string;
+    title: string;
+    column5: string | number;
+    url: string;
+    pubYear: string | number;
+    audience: string;
+    categories: string;
+    tag1: string;
+    tag2: string;
+    tag3: string;
+    miLevelFilters: string | number;
+    miAreaFilters: string | number;
+    miNotesFilters:string | number;
+    region: string;
+    status: string | number;
+}
